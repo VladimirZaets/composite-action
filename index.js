c9a6491678ed23ab4f9a8a868cc30af0e827eb32
Volodymyr Zaets <vzaets@magento.com>
2021-12-08 16:20:53 -0600

    test#14

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const sodium = require('tweetsodium');
 const github = require('@actions/github');
 
-const octokit = github.getOctokit(process.argv[3])
-const publicKeyData = await octokit.rest.actions.getRepoPublicKey({
-    owner: 'VladimirZaets',
-    repo: 'postMessages'
-})
-console.log(JSON.stringify(publicKeyData))
-const key = "VtIgSKa0V1oKVr/w7E2GloJU9tZndwrpJ3tfuEIJkUk=";
-const value = process.argv[2];
-console.log(value);
-const messageBytes = Buffer.from(value);
-const keyBytes = Buffer.from(key, 'base64');
+async function run () {
+
+    const octokit = github.getOctokit(process.argv[3])
+    const publicKeyData = await octokit.rest.actions.getRepoPublicKey({
+        owner: 'VladimirZaets',
+        repo: 'postMessages'
+    })
+    console.log(JSON.stringify(publicKeyData))
+    const key = "VtIgSKa0V1oKVr/w7E2GloJU9tZndwrpJ3tfuEIJkUk=";
+    const value = process.argv[2];
+    console.log(value);
+    const messageBytes = Buffer.from(value);
+    const keyBytes = Buffer.from(key, 'base64');
 
 
 // Encrypt using LibSodium.
-const encryptedBytes = sodium.seal(messageBytes, keyBytes);
-
+    const encryptedBytes = sodium.seal(messageBytes, keyBytes);
+    const encrypted = Buffer.from(encryptedBytes).toString('base64');
+}
 
 // Base64 the encrypted secret
-const encrypted = Buffer.from(encryptedBytes).toString('base64');
 
-console.log(encrypted)
