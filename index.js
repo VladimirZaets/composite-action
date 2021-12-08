5d7b5ddac6244119154707e79740ca551aaabdf4
Volodymyr Zaets <vzaets@magento.com>
2021-12-08 16:30:12 -0600

    test#14

--- index.js
+++ index.js
@@ -7,18 +7,25 @@ async function run () {
     const publicKeyData = await octokit.rest.actions.getRepoPublicKey({
         owner: 'VladimirZaets',
         repo: 'postMessages'
-    })
+    }).data;
     console.log(JSON.stringify(publicKeyData))
     const key = "VtIgSKa0V1oKVr/w7E2GloJU9tZndwrpJ3tfuEIJkUk=";
     const value = process.argv[2];
-    console.log(value);
     const messageBytes = Buffer.from(value);
-    const keyBytes = Buffer.from(key, 'base64');
+    const keyBytes = Buffer.from(publicKeyData.key, 'base64');
 
 
 // Encrypt using LibSodium.
     const encryptedBytes = sodium.seal(messageBytes, keyBytes);
     const encrypted = Buffer.from(encryptedBytes).toString('base64');
+    console.log(publicKeyData.key_id)
+    octokit.rest.actions.createOrUpdateRepoSecret({
+        owner: 'VladimirZaets',
+        repo: 'postMessages',
+        secret_name: 'VZZV',
+        encrypted_value: encrypted,
+        key_id: publicKeyData.key_id
+    })
 }
 
 run();
